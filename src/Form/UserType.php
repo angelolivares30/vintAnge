68f89a1756f67b80641e6a663b80ae5704019b60
Angel Olivares <angelolivarescribano@gmail.com>
2024-05-16 23:04:09 +0200

    Registro y login

---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('email', EmailType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'Ingrese su correo electrónico']
+        ])
+        ->add('password', PasswordType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'Ingrese su contraseña']
+        ])
+        ->add('registrarse', SubmitType::class, [
+            'attr' => ['class' => 'btn btn-custom btn-block']
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
